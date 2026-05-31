7c886d559867937a720c00ad9ef269d17dad0c21
n12420727 <n12420727@qut.edu.au>
2026-05-31 14:50:33 +1000

    Cleaning up of folders and file management
    Removed most unused files and separated scenes and other assets into a group member basis.

--- Assets/read me.cs
+++
@@ -1,4 +0,0 @@
-// the pro builder stuff i created consists of the doorway and the ship. The door way is meant to look like that
-// so that when you walk through the colours zip around. I also created the arch thing with pro builder as well as the
-// massive floating thing pointing at the player. Just wanted to clarifye what I had created. everything else I either imported from
-// online or created myslef. Also press E ;)
